7fc9045834fef92d21f6fcc62f19bdd7749e3158
kirinrastogi <rastogikirin@gmail.com>
2016-11-19 16:29:45 -0500

    Refactoring

--- models/HeadlineModel.js
+++ models/HeadlineModel.js
@@ -6,7 +6,7 @@ const headlineSchema = new Schema({
     title: { type: String, required: true },
 	description: { type: String, required: true },
 	url: { type: String, required: true },
-    urlToImage: { type: String }
+    urlToImage: { type: String },
     votes: { type: Number, required: true },
     publishedAt: { type: String }
 })
