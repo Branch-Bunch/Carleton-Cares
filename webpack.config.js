9a1e14b2b9d72d8723914d8062fee1a4cbed04be
Jacky Chiu <jacky.chiu888@gmail.com>
2016-11-19 16:14:46 -0500

    React working now, webpack + babel use

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './app.js',
-  output: { path: __dirname, filename: 'bundle.js'},
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  }
-};﻿
+    entry: './public/js/index.js',
+    output: { path: __dirname, filename: './public/js/bundle.js'},
+    watch: true,
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
