b15c4c1304fb6bafe9cc3ea3e9b17376ce8ac082
Jacky Chiu <jacky.chiu888@gmail.com>
2016-12-06 19:34:28 -0500

    Added actions for flux dataflow

---
+++ public/js/actions/ArticleActions.js
@@ -0,0 +1,43 @@
+import dispatcher from '../dispatcher.js'
+
+export default class ArticleActions {
+    // TODO: Have different fetch calls for top/hot articles
+    static fetchArticles() {
+        return new Promise((resolve, reject) => {
+            fetch(`articles`)
+                .then(res => res.json())
+                .then((articles) => {
+                    articles = articles.sort((a, b) => b.votes - a.votes)
+                    dispatcher.dispatch({
+                        articles,
+                        type: 'UPDATE_ARTICLES'
+                    })
+                    resolve()
+                })
+                .catch((err) =>  {
+                    console.log('Error fetching articles', err)
+                    reject(err)
+                })
+        })
+    }
+
+    static postVote(vote, id) {
+        return new Promise((resolve, reject) => {
+            fetch('articles/vote', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    vote,
+                    id
+                })
+            })
+            .then(res => resolve(res))
+            .catch((err) => {
+                console.log('Vote failed to respond')
+                reject(err)
+            })
+        })
+    }
+} 
