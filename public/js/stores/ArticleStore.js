b0d4616913097cb05665f539498f24133bda75ca
Jacky Chiu <jacky.chiu888@gmail.com>
2016-12-06 19:34:59 -0500

    Added data store for articles

---
+++ public/js/stores/ArticleStore.js
@@ -0,0 +1,29 @@
+import { EventEmitter } from 'events'
+import dispatcher from '../dispatcher.js'
+
+class ArticleStore extends EventEmitter {
+    constructor() {
+        super()
+        this.articles = []
+        this.handleAction = this.handleAction.bind(this)
+    }
+
+    getArticles() {
+        return this.articles
+    }
+
+    handleAction(action) {
+        console.log(action)
+        switch(action.type) {
+            case 'UPDATE_ARTICLES': {
+                this.articles = action.articles 
+                this.emit('update')
+                break
+            }
+        }
+    }
+}
+
+const articleStore = new ArticleStore()
+dispatcher.register(articleStore.handleAction)
+export default articleStore
