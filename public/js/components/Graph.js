eda374d736c067ffd613405a6aa0d5671ea9a1f0
vikrambombhi <vbombhi@gmail.com>
2017-01-02 22:00:44 -0500

    code cleanup and commenting

--- public/js/components/Graph.js
+++ public/js/components/Graph.js
@@ -6,7 +6,7 @@ export default class Graph extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            votes: []
+            dataPoints: []
         }
     }
 
@@ -16,24 +16,25 @@ export default class Graph extends React.Component {
                 return res.json()
             })
             .then(words => {
-                const dataPoints = words[0].votes 
+                // for now graphing first word associated with the top article
+                // createing array of dataPoints
+                const dataPoints = words[0].votes
                     .map(point => [point.time, point.sum])
-			
+                // adding axis to dataPoints array
 				dataPoints.unshift(['Time', words[0].word])
                 this.setState({
-					votes: dataPoints
+					dataPoints: dataPoints
 				})
             })
             .catch(err => console.log('Error fetching articles', err))
     }
 
   render() {
-		console.log(this.state.votes)
 		return (
 			<div>
                 <Chart
                   chartType="AreaChart"
-                  data={this.state.votes}
+                  data={this.state.dataPoints}
 				  options={{}}
                   graph_id="ScatterChart"
                   width="100%"
