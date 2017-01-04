af48ae1caf194ed94bc58112e09e0b6e4d1670e6
vikrambombhi <vbombhi@gmail.com>
2017-01-04 18:42:38 -0500

    requested changes, everything is good to go now. Renamed article test file

--- routes/keyword.js
+++ routes/keyword.js
@@ -7,30 +7,14 @@ const Keyword = require('../models/KeywordModel.js')
 
 const router = express.Router()
 
-
-router.get('/:word', (req, res) => {
-    let word = req.params.word
-    Keyword.find({word: word})
-    .lean()
-    .then((wordList) => {
-        res.send(wordList[0].votes)
-    })
-    .catch((err) => {
-        res.status(500).send({
-            err,
-            givens: req.params
-        })
-    })
-})
-
 router.get('/top', (req, res) => {
 	Keyword
 		.find()
 		.lean()
 		.then((wordList) => {
 			const topWords = wordList.reduce((accumulator, word) => {
-				let wordSum = word.votes[word.votes.length-1].sum
-				let topSum = accumulator.sum
+				const wordSum = word.votes[word.votes.length-1].sum
+				const topSum = accumulator.sum
 				if (wordSum > topSum) {
 					accumulator.words = [word]
 					accumulator.sum = wordSum
@@ -52,6 +36,21 @@ router.get('/top', (req, res) => {
                 givens: req.query
             })
 		})
+})
+
+router.get('/:word', (req, res) => {
+    let word = req.params.word
+    Keyword.find({word: word})
+    .lean()
+    .then((wordList) => {
+        res.send(wordList[0].votes)
+    })
+    .catch((err) => {
+        res.status(500).send({
+            err,
+            givens: req.params
+        })
+    })
 })
 
 module.exports = router
