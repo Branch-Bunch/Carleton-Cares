9968bd835367b02b8ec535264336ba21a7509d75
kirinrastogi <rastogikirin@gmail.com>
2017-01-05 11:18:42 -0500

    Fix ArticleModel tabbing

--- models/ArticleModel.js
+++ models/ArticleModel.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema
 const headlineSchema = new Schema({
     author: { type: String },
     title: { type: String, required: true },
-	description: { type: String, required: true },
-	url: { type: String, required: true },
+    description: { type: String, required: true },
+    url: { type: String, required: true },
     urlToImage: { type: String },
     votes: { type: Number, required: true },
     publishedAt: { type: String },
