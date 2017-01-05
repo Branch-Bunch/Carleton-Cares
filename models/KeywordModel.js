a1ee0cf4b86c35f9160ca7fe8d3f7e53eb157d51
kirinrastogi <rastogikirin@gmail.com>
2017-01-05 11:19:03 -0500

    Fix keywordmodel tabbing

--- models/KeywordModel.js
+++ models/KeywordModel.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema
 
 const voteSchema = new Schema({
     sum: { type: Number, required: true },
-	time: { type: String, required: true }
+    time: { type: String, required: true }
 }, {_id: false})
 
 const keywordSchema = new Schema({
-	word: { type: String, lowercase: true, required: true },
-	votes: [voteSchema]
+    word: { type: String, lowercase: true, required: true },
+    votes: [voteSchema]
 })
 
 const Keyword = mongoose.model('Keyword', keywordSchema, 'Keyword')
