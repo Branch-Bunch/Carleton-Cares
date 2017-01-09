fffe6b106353577d0be87fd265fbfab5add0104f
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-09 09:05:04 -0500

    Added node env in webpack config to know ahead of time for minifiy react

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
     }],
   },
   plugins: debug ? [] : [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
