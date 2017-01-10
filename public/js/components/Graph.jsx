453a0305c35f6a04e9fac69486b4d37553de59e6
vikrambombhi <vbombhi@gmail.com>
2017-01-09 20:00:04 -0500

    jslint cleanup

--- public/js/components/Graph.jsx
+++ public/js/components/Graph.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
-import {render} from 'react-dom'
-import {Chart} from 'react-google-charts'
+import { Chart } from 'react-google-charts'
 import GraphStore from '../stores/GraphStore'
 import GraphActions from '../actions/GraphActions'
 
 export default class Graph extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            dataPoints: GraphStore.getDataPoints()
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      dataPoints: GraphStore.getDataPoints(),
     }
+  }
 
-    componentDidMount() {
-        GraphStore.on('graphUpdate', () => {
-            this.setState({
-                dataPoints: GraphStore.getDataPoints()
-            })
-        })
-        GraphActions.graphTop()
-    }
+  componentDidMount() {
+    GraphStore.on('graphUpdate', () => {
+      this.setState({
+        dataPoints: GraphStore.getDataPoints(),
+      })
+    })
+    GraphActions.graphTop()
+  }
+
+  render() {
+    return (
+      <div>
+        <Chart
+          chartType="AreaChart"
+          data={this.state.dataPoints}
+          options={{}}
+          graph_id="ScatterChart"
+          width="100%"
+          height="400px"
+          legend_toggle
+        />
+      </div>
+    )
+  }
 
 
-    render() {
-        return (
-            <div>
-                <Chart
-                  chartType="AreaChart"
-                  data={this.state.dataPoints}
-				          options={{}}
-                  graph_id="ScatterChart"
-                  width="100%"
-                  height="400px"
-                  legend_toggle
-                 />
-            </div>
-        )
-    }
 }
--- routes/keyword.js
+++ routes/keyword.js
@@ -1,61 +1,57 @@
-'use strict'
-
 const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const Keyword = require('../models/KeywordModel.js')
+const Keyword = require('../models/KeywordModel')
 
 const router = express.Router()
 
 router.get('/top', (req, res) => {
-	Keyword
-		.find()
-		.lean()
-		.then((wordList) => {
-			const topWords = wordList.reduce((accumulator, word) => {
-				const wordSum = word.votes[word.votes.length-1].sum
-				const topSum = accumulator.sum
-				if (wordSum > topSum) {
-					accumulator.words = [word]
-					accumulator.sum = wordSum
-				}
-				if (wordSum === topSum) {
-					accumulator.words.push(word)
-				}
-				return accumulator
-			}, {
-				sum: 0,
-				words: []
-			})
-			res.send(topWords.words)
-		})
-		.catch((err) => {
-			console.log(err)
-			res.status(500).send({
-                err,
-                givens: req.query
-            })
-		})
+  Keyword
+    .find()
+    .lean()
+    .then((wordList) => {
+      const topWords = wordList.reduce((accumulator, word) => {
+        const wordSum = word.votes[word.votes.length - 1].sum
+        const topSum = accumulator.sum
+        if (wordSum > topSum) {
+          return {
+            words: [word],
+            sum: wordSum,
+          }
+        }
+        if (wordSum === topSum) {
+          accumulator.words.push(word)
+        }
+        return accumulator
+      }, {
+        sum: 0,
+        words: [],
+      })
+      res.send(topWords.words)
+    })
+    .catch((err) => {
+      res.status(500).send({
+        err,
+        givens: req.query,
+      })
+    })
 })
 
 router.get('/:word', (req, res) => {
-    let word = req.params.word
-    Keyword.find({word: word})
+  const word = req.params.word
+  Keyword.find({ word })
     .lean()
     .then((wordList) => {
-		if(wordList.length === 0){
-			res.status(500).send({
-	            err: 'word not found',
-	            givens: req.params
-	        })
-		}
-		else res.send(wordList)
-	})
-    .catch((err) => {
+      if (wordList.length === 0) {
         res.status(500).send({
-            err,
-            givens: req.params
+          err: 'word not found',
+          givens: req.params,
         })
+      } else res.send(wordList)
+    })
+    .catch((err) => {
+      res.status(500).send({
+        err,
+        givens: req.params,
+      })
     })
 })
 
