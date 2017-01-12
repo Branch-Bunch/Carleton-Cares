ea92b3077c070210a07b350ba3edcff4d5a19ad1
kirinrastogi <rastogikirin@gmail.com>
2017-01-12 20:20:29 +0000

    Refactor incrementKeyword to use catch exclusively for error handling

--- routes/keyword.js
+++ routes/keyword.js
@@ -5,84 +5,72 @@ const router = express.Router()
 
 router.get('/top', (req, res) => {
   Keyword.find()
-    .lean()
-    .then((wordList) => {
-      const topWords = wordList.reduce((accumulator, word) => {
-        const wordSum = word.votes[word.votes.length - 1].sum
-        const topSum = accumulator.sum
-        if (wordSum > topSum) {
-          return {
-            words: [word],
-            sum: wordSum,
-          }
-        }
-        if (wordSum === topSum) {
-          accumulator.words.push(word)
-        }
-        return accumulator
-      }, {
-        sum: 0,
-        words: [],
-      })
-      res.send(topWords.words)
-    })
-    .catch((err) => {
-      res.status(500).send({
-        err,
-        givens: req.query,
-      })
-    })
+        .lean()
+        .then((wordList) => {
+          const topWords = wordList.reduce((accumulator, word) => {
+            const wordSum = word.votes[word.votes.length - 1].sum
+            const topSum = accumulator.sum
+            if (wordSum > topSum) {
+              return {
+                words: [word],
+                sum: wordSum,
+              }
+            }
+            if (wordSum === topSum) {
+              accumulator.words.push(word)
+            }
+            return accumulator
+          }, {
+            sum: 0,
+            words: [],
+          })
+          res.send(topWords.words)
+        })
+        .catch((err) => {
+          res.status(500).send({
+            err,
+            givens: req.query,
+          })
+        })
 })
 
 router.get('/:word', (req, res) => {
   const word = req.params.word
   Keyword.findOne({ word })
-    .lean()
-    .then((keyword) => {
-      if (!keyword) throw new Error('Word not found')
-      res.send([keyword])
-    })
-    .catch((err) => {
-      res.status(500).send({
-        err: err.toString(),
-        givens: req.params,
-      })
-    })
+        .lean()
+        .then((keyword) => {
+          if (!keyword) throw new Error('Word not found')
+          res.send([keyword])
+        })
+        .catch((err) => {
+          res.status(500).send({
+            err: err.toString(),
+            givens: req.params,
+          })
+        })
 })
 
 function incrementKeyword(word, amount) {
   return new Promise((resolve) => {
     Keyword.findOne({ word })
             .then((keyword) => {
-              if (!keyword) throw new Error('Keywords is not array')
-              const votes = keyword.votes
-              const len = votes.length
-              const oldSum = keyword.votes[len - 1].sum
-              const newSum = oldSum + amount
-
-              const newVote = {
-                sum: newSum,
-                time: Date.now(),
+              let oldSum = 0
+              if (!keyword) {
+                keyword = new Keyword({
+                  word,
+                  votes: [],
+                })
+              } else {
+                const len = keyword.votes.length
+                oldSum = keyword.votes[len - 1].sum
               }
-              votes.push(newVote)
+              const newSum = oldSum + amount
+              const newVote = { sum: newSum, time: Date.now() }
+              keyword.votes.push(newVote)
               keyword.save()
               resolve({ word: keyword.word, newSum, oldSum, id: keyword.id })
             })
-            .catch(() => {
-              const oldSum = 0
-              const newSum = oldSum + amount
-              const newVote = {
-                sum: newSum,
-                time: Date.now(),
-              }
-
-              const keyword = new Keyword({
-                word,
-                votes: [newVote],
-              })
-              keyword.save()
-              resolve({ word: keyword.word, newSum, oldSum, id: keyword.id })
-            })
+            .catch(err => new Error(err.message))
   })
 }
 
