259d61e2c2c739332a95d352bfaa73be3ab525b3
kirinrastogi <rastogikirin@gmail.com>
2017-02-13 12:48:43 +0000

    Add base for upserting unit tests

--- test/fetchArticles.test.js
+++ test/fetchArticles.test.js
@@ -28,4 +28,29 @@ describe('fetchArticles unit tests', () => {
       sanitize(testString).should.not.equal(testString)
     })
   })
+
+  describe('upserting the articles', () => {
+    const testArticle = {
+      author: 'JSON Bourne',
+      title: 'JSON Bourne returns',
+      description: 'Jesus Christ, is that JSON Bourne?',
+      url: 'some-url',
+      urlToImage: 'https://upload.wikimedia.org/wikipedia/en/6/60/Jason_bourne_infobox.jpg',
+      publishedAt: '0001-01-01T00:00:00Z',
+      keywords: [ 'JSON-Bourne', 'movies' ],
+    }
+
+    it('should have a recent date if the date supplied is before 2000', (done) => {
+
+    })
+
+    it('should have the votes property added, and initialized to 0', (done) => {
+
+    })
+
+    it('should exist in the database after being upserted', (done) => {
+
+    })
+
+  })
 })
