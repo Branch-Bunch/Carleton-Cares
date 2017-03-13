35a75bc8a82002a9ddc8c9218994ad9d95e6cc51
Tuanhao <tuanhao256@gmail.com>
2017-03-13 12:08:36 -0400

    sorting bar buttons remains active if clicked

--- public/js/components/SortingBar.jsx
+++ public/js/components/SortingBar.jsx
@@ -9,18 +9,36 @@ export default class SortingBar extends React.Component {
     ArticleActions.fetchArticles(sort)
   }
 
+  constructor() {
+    super()
+    this.state = {
+      newActive: null,
+      topActive: null,
+    }
+  }
+
+  setNewActive() {
+    this.setState({ newActive: true, topActive: null })
+  }
+
+  setTopActive() {
+    this.setState({ newActive: null, topActive: true })
+  }
+
   render() {
     return (
       <ButtonGroup>
         <Button
-          onClick={() => SortingBar.changeSort('NEW')}
+          onClick={() => { SortingBar.changeSort('NEW'); this.setNewActive() }}
           bsStyle="info"
           bsSize="small"
+          active={this.state.newActive}
         >Newest</Button>
         <Button
-          onClick={() => SortingBar.changeSort('TOP')}
+          onClick={() => { SortingBar.changeSort('TOP'); this.setTopActive() }}
           bsStyle="info"
           bsSize="small"
+          active={this.state.topActive}
         >Top</Button>
         <Button
           onClick={() => alert('Trending is not avaiable yet')}
